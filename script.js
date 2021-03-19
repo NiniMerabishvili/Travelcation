d46859a38a479366264b72ff4757fb71d7ca0486
NiniMerabishvili <ninni.merabishvili@gmail.com>
2021-03-19 18:47:26 +0400

    Home changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function() {
     $(window).on("scroll", function() {
-        if($(window).scrollTop() > 800) {
+        if($(window).scrollTop() > 50) {
             $(".container").addClass("active");
         } else {
             //remove the background property so it comes transparent again (defined in your css)
