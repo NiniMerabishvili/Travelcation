b5492417c6787f523f2bfd8127dee7ab053a8def
NiniMerabishvili <ninni.merabishvili@gmail.com>
2021-04-05 00:20:34 +0400

    Responsive, About us

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ $(function() {
     });
 });
 
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".container1").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".navigation-fixed").removeClass("active");
+        }
+    });
+});
+
+
   
 $("#burger-menu").on("click", function(){
     $("#menu").toggleClass("open-menu");
